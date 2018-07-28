37a742e84ba0cb845fa46ceeb2772f8705bd5bc3
Ryan Moore <moorer@udel.edu>
2018-07-28 03:04:07 -0400

    Add viral host matcher

--- lib/big_simon.rb
+++ lib/big_simon.rb
@@ -9,9 +9,42 @@ module BigSimon
 
   # Project directories
   ROOT       = File.join __dir__, ".."
-  BIN        = File.join ROOT, "vendor", "bin"
+  BIN        = File.join ROOT, "vendor", "bin", "mac"
   SPEC       = File.join ROOT, "spec"
   TEST_FILES = File.join SPEC, "test_files"
+
+  class Parsers
+
+    def self.vir_host_matcher fname
+      hosts = nil
+
+      host_info = {}
+      File.open(fname, "rt").each_line.with_index do |line, idx|
+        line.chomp!
+        line.sub! /,$/, "" # git rid of trailing commas
+
+        if idx.zero?
+          stat, *hosts = line.split ","
+        else
+          ary   = line.split ","
+          virus = ary.shift
+
+          dists = ary.map.
+              with_index { |dist, idx| [hosts[idx], dist.to_f] }.
+              sort_by { |_, dist| dist }
+
+          best_host = dists[0][0]
+
+          host_info[virus] = {
+              best: best_host,
+              all:  dists
+          }
+        end
+      end
+
+      host_info
+    end
+  end
 
   class Runners
 
@@ -40,6 +73,28 @@ module BigSimon
       Process.run_and_time_it! "Predicting host", predict
 
       FileUtils.rm_r model_dir if Dir.exist? model_dir
+    end
+
+    def self.vir_host_matcher exe, vir_dir, host_dir, outdir
+      FileUtils.mkdir_p outdir
+
+      cmd = "python #{exe} " \
+      "-v #{vir_dir} " \
+      "-b #{host_dir} " \
+      "-o #{outdir} " \
+      "-d 1" # only compute d2star dissimilarity
+
+      Process.run_and_time_it! "Computing d2star dissimilarity", cmd
+
+      tmp_dir = File.join outdir, "tmp"
+      FileUtils.rm_r tmp_dir if Dir.exist? tmp_dir
+
+      bad_files = %w[d2star_k6_main.html hostTaxa.txt_new.txt]
+      bad_files.each do |fname|
+        path = File.join outdir, fname
+
+        FileUtils.rm path if File.exist? path
+      end
     end
   end
 end
--- spec/big_simon_spec.rb
+++ spec/big_simon_spec.rb
@@ -3,14 +3,41 @@ RSpec.describe BigSimon do
     expect(BigSimon::VERSION).not_to be nil
   end
 
+  describe BigSimon::Parsers do
+    describe "::vir_host_matcher" do
+      let(:fname) { File.join BigSimon::TEST_FILES, "d2star_k6.csv" }
+
+      it "parses vir_host_matcher output" do
+        result   = BigSimon::Parsers.vir_host_matcher fname
+        expected = {
+            "AJ609634.fasta" => {
+                best: "NC_002971.fasta",
+                all:  [["NC_002971.fasta", 0.365838], ["NC_002163.fasta", 0.380042]]
+            },
+            "DQ113772.fasta" => {
+                best: "NC_002971.fasta",
+                all:  [["NC_002971.fasta", 0.360301], ["NC_002163.fasta", 0.404101]]
+            },
+        }
+
+        expect(result).to eq expected
+      end
+
+      context "bad input data" do
+        it "handles duplicate virus names"
+        it "handles duplicate host names"
+      end
+    end
+  end
+
   describe BigSimon::Runners do
     let(:exe) { File.join BigSimon::BIN, program_name }
     let(:outdir) { File.join BigSimon::TEST_FILES, "test_output", program_name }
-    let(:vir_dir) { File.join BigSimon::TEST_FILES, program_name, "virus" }
-    let(:host_dir) { File.join BigSimon::TEST_FILES, program_name, "host" }
+    let(:vir_dir) { File.join BigSimon::TEST_FILES, "virus" }
+    let(:host_dir) { File.join BigSimon::TEST_FILES, "host" }
     let(:threads) { 3 }
 
-    describe "#wish" do
+    describe "::wish" do
       let(:program_name) { "WIsH" }
 
       it "removes model dir" do
@@ -21,6 +48,20 @@ RSpec.describe BigSimon do
         }.not_to raise_error
 
         expect(File).not_to exist(File.join outdir, "model")
+      end
+    end
+
+    describe "::vir_host_matcher" do
+      let(:program_name) { "vhm.py" }
+
+      it "deletes the extra stuff" do
+        expect {
+          BigSimon::Runners.vir_host_matcher exe, vir_dir, host_dir, outdir
+        }.not_to raise_error
+
+        expect(File).not_to exist(File.join outdir, "tmp")
+        expect(File).not_to exist(File.join outdir, "d2star_k6_main.html")
+        expect(File).not_to exist(File.join outdir, "hostTaxa.txt_new.txt")
       end
     end
   end
